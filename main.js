acdf2b451eca2aaa208c5a7e307e770d889d5c54
Kongara Anshu <75679096+AeROsTrO@users.noreply.github.com>
2022-06-26 22:09:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const matchList=document.getElementById('match-list');
 //search states.json and filter it
 const searchStates=async searchText=>{
 const res=await fetch('info.json');
-const states=await res.json();
+const stas=await res.json();
 // console.log(states);//outputs all states 
 //get matches to current text inputs
-let matches=states.filter(state=>{
+let matches=stas.filter((state)=>{
     const regex=new RegExp(`^${searchText}`, 'gi');
-    return state.name.match(regex)||state.abbr.match(regex);
+    return state.name.match(regex)||state.code.match(regex);
+    // console.log(state);
 });
 if(searchText.length==0){
     matches=[];
@@ -21,9 +22,9 @@ const outputHtml=matches=>{
     if(matches.length>0){
         const html=matches.map(match=>`
         <div class="card">
-            <h4><span class="hi">${match.name}(${match.abbr})</span>
+            <h4><span class="hi">${match.name}(${match.code})</span>
             <span class="tct">${match.capital}</span></h4>
-            <small>Lat: ${match.lat} / Long: ${match.long}</small>
+            <small>Lat: ${match.coordinates[0]} / Long: ${match.coordinates[1]}</small>
         </div>
         `).join('');
         // console.log(html);
